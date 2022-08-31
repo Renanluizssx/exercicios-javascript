5d03be2f60db1726df2fb585b775a1c663e0b487
Renan Luiz <renanluizssx@gmail.com>
2022-08-31 07:48:38 -0300

    commitado

--- template.js
+++
@@ -1,8 +0,0 @@
-//template strings
-let nome = "Renan";
-let profissao = "Programador";
-
-
-// "" = para escrever uma string comum `` = para chamar o template string  
-
-console.log (`meu nome é ${nome} e eu sou um ${profissao}`);
