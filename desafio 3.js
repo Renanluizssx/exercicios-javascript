26b97326ccb225ac241d5464cdace962bbd8f0da
Renan Luiz <renanluizssx@gmail.com>
2022-09-02 18:38:29 -0300

    commitado

--- desafio 3.js
+++ desafio 3.js
@@ -1,8 +1,17 @@
-
+// if tenário
 function personagemMorreu(dano, saude) {
     return saude < dano // CONDICAO
-    ? console.log("Personagem morreu") // SE (IF)
+    ?  console.log("Personagemmorreu") // SE (IF)
     : console.log("Personagem está vivo"); // SENAO (ELSE)
     }
    personagemMorreu(9, 2)
+
+
+function maiormenor (maior, menor) {
+    return maior > menor
+    ? console.log('O número é maior')
+    : console.log('O número é menor')
+}
+
+maiormenor(9, 10)
    
